ed590489405c8b54948c20b4859fc5e649ee4194
KyleTruong <ktruong008@gmail.com>
2023-05-31 20:21:33 -0400

    rename episode to post

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
   import Container from '$lib/shared/components/container.svelte';
-  import EpisodeEntry from './episode-entry.svelte';
+  import Post from './post.svelte';
 
   let episodes = [
     {
@@ -20,13 +20,13 @@
 
 <div class="pb-12 pt-16 sm:pb-4 lg:pt-12">
   <Container>
-    <h1 class="text-2xl font-bold leading-7 text-slate-900">Episodes</h1>
+    <h1 class="text-2xl font-bold leading-7 text-slate-900">Thoughts</h1>
   </Container>
   <div
     class="divide-y divide-slate-100 sm:mt-4 lg:mt-8 lg:border-t lg:border-slate-100"
   >
     {#each episodes as episode}
-      <EpisodeEntry {episode} />
+      <Post {episode} />
     {/each}
   </div>
 </div>
